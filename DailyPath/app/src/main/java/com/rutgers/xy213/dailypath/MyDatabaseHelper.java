00b916bd44d3aeda4cd988a74169fa9c43523d8f
Xin Yang <yx654402886@gmail.com>
2018-01-28 11:38:21 -0500

    DailyPath upload

---
+++ DailyPath/app/src/main/java/com/rutgers/xy213/dailypath/MyDatabaseHelper.java
@@ -0,0 +1,36 @@
+package com.rutgers.xy213.dailypath;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by yx960203 on 2017/11/6.
+ */
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+    private Context mContext;
+
+    public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        mContext = context;
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) //first time create table
+    {
+        db.execSQL("create table location(_id integer primary key autoincrement, address varchar, time integer, longitude varchar, latitude varchar, name varchar, marker varchar, associated varchar, named varchar)");//create database when first create
+       // db.execSQL("create table relation(_id integer primary key autoincrement, address varchar, parentaddress varchar, parent name)");//create database when first create
+
+     //   db.execSQL("create table fence(_id integer primary key autoincrement, fence varchar, enterflag int, exitflag int)");//create database when first create
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        //when upgrade
+    }
+
+}
